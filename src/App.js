352f6453e25fa257e8828e082eff08d78923d6b8
DarkOutlaw101 <james@isger.co.uk>
2020-07-04 13:26:03 +0100

    Initial prototype

--- src/App.js
+++ src/App.js
@@ -79,29 +79,63 @@ const App = () => {
         lastObj[last] = value;
     }
 
-    console.log("RERENDER", config)
+    console.log("RERENDER", config);
+
+    const labelStyle = "block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2";
+    const inputStyle = "appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500";
+    const headerStyle = "text-center font-semibold";
+
     return <React.Fragment>
-        <form>
-            <h1>Core</h1>
-            Root Directory: <input type={"text"} onChange={(e) => handleChange(e)} name={"app.core.root_directory"} value={config.app.core.root_directory} /> <br />
-            Log Level: <input type={"text"} onChange={(e) => handleChange(e)} name={"app.core.log_level"} value={config.app.core.log_level} /> <br />
-            Rate limit: <input type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rate_limit"} value={config.app.core.rate_limit} /> <br />
-            <h1>Database</h1>
-            Path: <input type={"text"} onChange={(e) => handleChange(e)} name={"app.core.db.path"} value={config.app.core.db.path} /> <br />
-            Initialised: <input type={"text"} onChange={(e) => handleChange(e)} name={"app.core.db.initialised"} value={config.app.core.db.initialised} /> <br />
-            <h1>Rest</h1>
-            Instance URL: <input type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rest.url"} value={config.app.core.rest.url} /> <br />
-            Username: <input type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rest.user"} value={config.app.core.rest.user} /> <br />
-            Password: <input type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rest.password"} value={config.app.core.rest.password} /> <br />
-            Masked: <input type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rest.masked"} value={config.app.core.rest.masked} /> <br />
-            Xor Key: <input type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rest.xor_key"} value={config.app.core.rest.xor_key} /> <br />
-            <h1>Tables</h1>
-            <p>In progress...</p>
+        <div className="flex">
+
+            <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 w-1/4 h-full">
+
+            <h1 className={headerStyle}>Core</h1>
+                <label className={labelStyle}>Root Directory:</label>
+                <input className={inputStyle} type={"text"} onChange={(e) => handleChange(e)} name={"app.core.root_directory"} value={config.app.core.root_directory} /> <br />
+
+                <label className={labelStyle}>Log Level: </label>
+                <input className={inputStyle} type={"text"} onChange={(e) => handleChange(e)} name={"app.core.log_level"} value={config.app.core.log_level} /> <br />
+
+                <label className={labelStyle}>Rate limit: </label>
+                <input className={inputStyle} type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rate_limit"} value={config.app.core.rate_limit} /> <br />
+
+            <h1 className={headerStyle}>Database</h1>
+                <label className={labelStyle}>Path: </label>
+                <input className={inputStyle} type={"text"} onChange={(e) => handleChange(e)} name={"app.core.db.path"} value={config.app.core.db.path} /> <br />
+
+                <label className={labelStyle}>Initialised: </label>
+                <input className={inputStyle} type={"text"} onChange={(e) => handleChange(e)} name={"app.core.db.initialised"} value={config.app.core.db.initialised} /> <br />
+
+
+            <h1 className={headerStyle}>Rest</h1>
+                <label className={labelStyle}> Instance URL: </label>
+                <input className={inputStyle} type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rest.url"} value={config.app.core.rest.url} /> <br />
+
+                <label className={labelStyle}>Username: </label>
+                <input className={inputStyle} type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rest.user"} value={config.app.core.rest.user} /> <br />
+
+                <label className={labelStyle}>Password: </label>
+                <input className={inputStyle} type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rest.password"} value={config.app.core.rest.password} type="password" /> <br />
+
+                <label className={labelStyle}>Masked: </label>
+                <input className={inputStyle} type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rest.masked"} value={config.app.core.rest.masked} /> <br />
+
+                <label className={labelStyle}>Xor Key: </label>
+                <input className={inputStyle} type={"text"} onChange={(e) => handleChange(e)} name={"app.core.rest.xor_key"} value={config.app.core.rest.xor_key} /> <br />
+
         </form>
-        {/* this here goes to the right */}
-        <pre>
-            {yaml.safeDump(config)}
-        </pre>
+
+            <div className="w-3/4">
+                <div className="bg-gray-100 md:mx-32 md:my-8 px-10 py-8 border border-gray-200 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500 shadow-md">
+                    <pre>
+                        {yaml.safeDump(config)}
+                    </pre>
+                </div>
+            </div>
+
+        </div>
+
     </React.Fragment>
 };
 
